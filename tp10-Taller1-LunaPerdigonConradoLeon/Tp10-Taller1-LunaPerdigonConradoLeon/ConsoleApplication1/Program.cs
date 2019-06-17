7be34296bb007216dbf35dacb41aee9c1e9ef67b
LeonLuna98 <you@example.com>
2019-06-17 20:47:56 -0300

    Practica para el parcial con tp10

---
+++ tp10-Taller1-LunaPerdigonConradoLeon/Tp10-Taller1-LunaPerdigonConradoLeon/ConsoleApplication1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Propiedad nuevaprop = cargardatos();
+
+            List<string> lalista = File.ReadAllLines(@"..\..\LasPropiedades.csv", Encoding.ASCII).ToList();//Mejor un arreglo(?
+            System.Console.WriteLine();
+            List<string> nuevalista = File.ReadAllLines(@"..\..\NuevaLista.csv", Encoding.ASCII).ToList();
+            foreach (string linea in lalista)
+            {
+                Propiedad propaux = cargardatos();
+                string aux = linea;
+                aux = aux + ";" + propaux.Tipooperacion.ToString() + ";";
+                aux = aux + propaux.Tamanio.ToString() + ";";
+                aux = aux + propaux.CantBanios.ToString() + ";";
+                aux = aux + propaux.CantHabitaciones.ToString() + ";";
+                aux = aux + propaux.Precio.ToString() + ";";
+                aux = aux + propaux.Estado.ToString();
+                nuevalista.Add(aux);    
+                File.WriteAllLines(@"..\..\NuevaLista.csv", nuevalista.ToArray());
+            }
+            Console.WriteLine("Se sobreescribieron exitosamente los datos");
+
+            
+            string[] discos = System.IO.Directory.GetLogicalDrives();
+            foreach (string eldisco in discos)
+            {
+
+                Console.WriteLine("Disponible el disco:" + eldisco);
+            }
+
+            Console.WriteLine(@"En que disco desea guardar? (COPIE TEXTUALMENTE EL DISCO EJ: E:\)");//YA ANDA
+            string discoelegido = Console.ReadLine();
+            foreach (string eldiscoelegido in discos)
+            {
+
+                try
+                {
+
+                    if (Equals(eldiscoelegido.ToLower(), discoelegido.ToLower()))
+                    {
+                        if (Directory.Exists(eldiscoelegido))
+                        {
+                            string elbackup = eldiscoelegido + @"\ BackUpDeLasPropiedades";
+                            Directory.CreateDirectory(elbackup);
+                            //File.Copy("archivoorigonal", "archivodestino.nuevaextension")
+                            string fecha = DateTime.Now.ToString(@"hh\mm\Ss");
+
+                            fecha = fecha.Replace(':', '-');
+                            fecha = fecha.Replace('\\', '_');
+                            string nombreArchivo = @"\BackUpPropiedades" + fecha + ".bk";
+                            File.Copy(@"..\..\NuevaLista.csv", elbackup + nombreArchivo);
+
+                            //File.Move(rutadelarchivo, elbackup + ".bk");
+                            Console.WriteLine("Se guardo exitosamente!");
+
+                        }
+                        else
+                        {
+                            string elbackup = eldiscoelegido + @"\ BackUp";
+                            Directory.CreateDirectory(elbackup);
+                            Console.WriteLine("La carpeta no existe, creando una...");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR: PROBLEMAS AL BUSCAR LA CARPETA ");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static Propiedad cargardatos()
+        {
+            string[] direcciones = { "Direccion1", "Direccion2", "Direccion3", "Direccion4", "Direccion5", "Direccion6" };
+           
+            Random rnd = new Random();
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+
+            string direccion= direcciones[rnd.Next(0,6)];
+
+            
+            int opcion = rnd.Next(1, 3);
+            TipoDeOperacion laoperacion;
+            switch (opcion)
+            {
+                case 1:
+                    laoperacion = TipoDeOperacion.Venta;
+                    break;
+                case 2:
+                    laoperacion = TipoDeOperacion.Alquiler;
+                    break;
+                default:
+                    laoperacion = TipoDeOperacion.Alquiler;
+                    break;
+            }
+
+            int opcion1 = rnd1.Next(1, 6);
+           TipoDePropiedad lapropiedad;
+            switch (opcion1)
+            {
+                case 1:
+                    lapropiedad= TipoDePropiedad.Departamento;
+                    break;
+                case 2:
+                    lapropiedad = TipoDePropiedad.Casa;
+                    break;
+                case 3:
+                    lapropiedad = TipoDePropiedad.Duplex;
+                    break;
+                case 4:
+                    lapropiedad = TipoDePropiedad.Penthhouse;
+                    break;
+                case 5:
+                    lapropiedad = TipoDePropiedad.Terreno;
+                    break;
+                default:
+                    lapropiedad = TipoDePropiedad.Departamento;
+                    break;
+
+            }
+              
+            int habitaciones = rnd.Next(1, 5);
+            int banio = rnd.Next(1, 3);
+            int elid = rnd.Next(1, 400);
+            float eltamanio = rnd1.Next(100, 2000);
+            int elprecio = rnd.Next(500000, 10000000);
+
+            bool elestado;
+            if (rnd.Next(0, 2) == 0)
+            {
+                elestado = false;
+            }
+            else
+            {
+                elestado = true;
+            }
+
+            Propiedad laprop = new Propiedad(elid,lapropiedad,laoperacion,eltamanio, banio, habitaciones, direccion, elprecio, elestado);
+            return laprop;
+        }
+    }
+}
---
+++ tp10-Taller1-LunaPerdigonConradoLeon/Tp10-Taller1-LunaPerdigonConradoLeon/ConsoleApplication1/Propiedad.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public enum TipoDeOperacion
+    {
+        Venta,Alquiler
+    }
+    public enum TipoDePropiedad
+    {
+        Departamento, Casa, Duplex, Penthhouse, Terreno
+    }
+
+    public class Propiedad
+    {
+        int id;
+        TipoDePropiedad tipopropiedad;
+        TipoDeOperacion tipooperacion;
+        float tamanio;
+        int cantBanios;
+        int cantHabitaciones;
+        string domicilio;
+        int precio;
+        bool estado;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+        public Propiedad(int _id, TipoDePropiedad _tipopropiedad, TipoDeOperacion _tipooperacion, float _tamanio, int _cantBanios, int _cantHabitaciones,string _domicilio, int _precio,bool _estado) { 
+            id = _id;
+            tipopropiedad = _tipopropiedad;
+            tipooperacion = _tipooperacion;
+            tamanio = _tamanio;
+            cantBanios = _cantBanios;
+            domicilio = _domicilio;
+            cantHabitaciones = _cantHabitaciones;
+            precio = _precio;
+            estado = _estado;
+
+        }
+
+
+
+        public void mostrar()
+        {
+            Console.WriteLine(Id);
+            Console.WriteLine(Tipopropiedad);
+            Console.WriteLine(Tipooperacion);
+            Console.WriteLine(Tamanio);
+            Console.WriteLine(CantBanios);
+            Console.WriteLine(CantHabitaciones);
+            Console.WriteLine(Domicilio);
+            Console.WriteLine(Precio);
+            Console.WriteLine(Estado);
+            Console.WriteLine("--------------");
+        }
+
+        public TipoDePropiedad Tipopropiedad
+        {
+            get
+            {
+                return tipopropiedad;
+            }
+
+            set
+            {
+                tipopropiedad = value;
+            }
+        }
+
+        public TipoDeOperacion Tipooperacion
+        {
+            get
+            {
+                return tipooperacion;
+            }
+
+            set
+            {
+                tipooperacion = value;
+            }
+        }
+
+        public float Tamanio
+        {
+            get
+            {
+                return tamanio;
+            }
+
+            set
+            {
+                tamanio = value;
+            }
+        }
+
+        public int CantBanios
+        {
+            get
+            {
+                return cantBanios;
+            }
+
+            set
+            {
+                cantBanios = value;
+            }
+        }
+
+        public int CantHabitaciones
+        {
+            get
+            {
+                return cantHabitaciones;
+            }
+
+            set
+            {
+                cantHabitaciones = value;
+            }
+        }
+
+        public string Domicilio
+        {
+            get
+            {
+                return domicilio;
+            }
+
+            set
+            {
+                domicilio = value;
+            }
+        }
+
+        public int Precio
+        {
+            get
+            {
+                return precio;
+            }
+
+            set
+            {
+                precio = value;
+            }
+        }
+
+        public bool Estado
+        {
+            get
+            {
+                return estado;
+            }
+
+            set
+            {
+                estado = value;
+            }
+        }
+
+        public void ValorDelInmueble()
+        {
+
+        double elprecio;
+            if (Tipooperacion == TipoDeOperacion.Venta)
+            {
+             
+                elprecio = precio + (precio*0.21) + (precio*0.10) + 10000;
+                int elprecioenint=Convert.ToInt32(elprecio);
+                Console.WriteLine("El valor del inmueble es {0}", elprecioenint);
+
+            }
+            else
+            {
+                elprecio =precio*0.02 + precio*0.005;
+                int elprecioenint = Convert.ToInt32(elprecio);
+                Console.WriteLine("El valor del inmueble es {0}", elprecioenint);
+
+            }
+        }
+    }
+}
